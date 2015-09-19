4dae454147b4219a60692dc2ea275ce0f46d67f5
Michael Reed <myk27x@yahoo.com>
2015-09-19 16:23:33 -0400

    updated /move url and added class methods to game

--- ttt_ftwww.rb
+++ ttt_ftwww.rb
@@ -1,23 +1,10 @@
+JSON_FILE_PLAYERS = File.dirname(__FILE__) + "/players.json"
+JSON_FILE_BOARD = File.dirname(__FILE__) + "/board.json"
+
 class TTTGame
 
   def initialize
-    @cells = [0,1,2,3,4,5,6,7,8]
-
-    @board = { "current_player": "choosing_player",
-             "status": "ok",
-             "board": { "state": "playing",
-                        "positions": { "0": @cells[0],
-                                       "1": @cells[1],
-                                       "2": @cells[2],
-                                       "3": @cells[3],
-                                       "4": @cells[4],
-                                       "5": @cells[5],
-                                       "6": @cells[6],
-                                       "7": @cells[7],
-                                       "8": @cells[8]
-                                     }
-                      }
-            }
+    @cells = [" "," "," "," "," "," "," "," "," "]
 
     @win2 = [
       [0,1,2],
@@ -30,149 +17,118 @@ class TTTGame
       [2,4,6]
     ]
   end
-end
 
-# require 'sinatra'
-require 'webrick'
-require 'json'
-
-JSON_FILE = File.dirname(__FILE__) + "/players.json"
-
-class Game < WEBrick::HTTPServlet::AbstractServlet
-  def do_POST(request, response)
-    TTTGame.new
-
-    ##TODO should players be class instances??? TODO##
-    body = JSON.parse(File.read("players.json"))
-      if body["player_X"] == 0
-        body["player_X"] = "#{request.query["playername"]}"
-      elsif body["player_O"] == 0
-        body["player_O"] = "#{request.query["playername"]}"
-      else
-
-        ##TODO send diff response??? TODO###
-        puts "already full"
-      end
-      ##TODO what tells the game to start (go to /move)? TODO###
-
-    File.write(JSON_FILE, body.to_json)
-
-    board = JSON.parse(File.read("board.json"))
-    response.status = 201
-    response.body = board.to_json
-  end
-end
-
-class Move
-  def do_POST(request, response)
-
-    # Request:
-    # {
-    #   "player": "X",
-    #   "position": "0"
-    # }
-
-    # If the move is invalid (the space is currently occupied)
-    # Status code: 409
-    #  {
-    #    "status": "invalid",
-    #    "reason": "Already an (X or O) at this space",
-    #    "board": { "state": "playing", etc.
-    ##TODO needs input variables from client TODO##
-
-    @board = {
-      "current_player": choosing_player,
+  def update_board_after_moves(choosing_player, status, state)
+    @board = { "current_player": choosing_player,
       "status": status,
       "board": { "state": state,
-                 "positions": { "0": @cells[0],
-                                "1": @cells[1],
-                                "2": @cells[2],
-                                "3": @cells[3],
-                                "4": @cells[4],
-                                "5": @cells[5],
-                                "6": @cells[6],
-                                "7": @cells[7],
-                                "8": @cells[8]
-                              }
-                }
-  }
+        "positions": { "0": @cells[0],
+          "1": @cells[1],
+          "2": @cells[2],
+          "3": @cells[3],
+          "4": @cells[4],
+          "5": @cells[5],
+          "6": @cells[6],
+          "7": @cells[7],
+          "8": @cells[8]
+        }
+      }
+    }
+  end
 
-
-  ## TODO can this be used to modify board?
-  ## TODO need return for invalid move
-  def make_move
-    @cells.map do |cell|
-      if cell == @p1_choice
-        @cells[@p1_choice] = :X
-        return true
-      end
+  def update_player
+    if @board[:"current_player"] == players[:"player_X"]
+      choosing_player = players[:"player_O"]
+    elsif @board["current_player"] == players[:"player_O"]
+      choosing_player = players[:"player_X"]
     end
   end
 
-## TODO modify for use on winner tally (incorporate with winner?)
-  def tally
+  def update_state_if_winner
     @win2.each do |inner|
-      inner.map! do |value|
-        if(value == @p1_choice)
-          value = :X
-        elsif(value == @p2_choice)
-          value = :O
-        elsif(value == @ai_choice)
-          value = :C
-        else
-          value
+      case
+      when
+        inner == [:X,:X,:X]
+        state = "Player X wins!"
+        # player X wins - player X has won #NOTE should this include name???
+      when
+        inner == [:O,:O,:O]
+        state = "Player O wins!"
+        # player Y wins - player Y has won #NOTE should this include name???
+      when
+        @cells.all? do |cell|
+          cell == " "
+          status = "tie"
         end
+      else
+        state = "playing"
       end
     end
   end
 
-## TODO modify for use // need return for winner?
-  def winner
-    @win2.each do |inner|
-      if inner == [:X,:X,:X]
-        puts "#{@name1} wins!"
-        play_again
-      elsif inner == [:O,:O,:O]
-        puts "#{@name2} wins!"
-        play_again
-      elsif inner == [:C,:C,:C]
-        puts "Computer wins!"
-        play_again
-      end
-    end
-  end
-
-# NOTE: need player name and position from URL
-# Request:
-# X player moves to position 0
-# Request:
-# {
-#   "player": "X",
-#   "position": "0"
-# }
-# If the move is valid (the space is currently unoccupied)
-# Status code: 200
-# Body:
-#  {
-#    "status": "ok",
-#    "board": { "state": "playing",
-#
-# NOTE: The board should be updated with an X or an O depending on which player made the move.
-# NOTE: The state should be updated with one of these values:
-
-# playing - the game is ongoing
-# tie - the game is a tie
-# player X wins - player X has won
-# player Y wins - player Y has won
-#
-  end
 end
 
+require 'sinatra'
+set :port, 3027
+require "sinatra/reloader" if development?
 
-server = WEBrick::HTTPServer.new(Port:3027)
-server.mount "/game", Game
-server.mount "/move", Move
+post '/game' do
+  @game = TTTGame.new
+  ##TODO should players be class instances??? TODO##
+  players = JSON.parse(File.read("players.json"))
+  if players["player_X"] == 0
+    players["player_X"] = "#{params["playername"]}"
+  elsif players["player_O"] == 0
+    players["player_O"] = "#{params["playername"]}"
+  else
+    ##TODO send diff response??? TODO###
+    puts "already full"
+  end
+  ##TODO what tells the game to start (go to /move)? TODO###
 
-trap("INT") { server.shutdown }
+  File.write(JSON_FILE_PLAYERS, players.to_json)
 
-server.start
+  choosing_player = players["player_X"]
+  status = "ok"
+  state = "playing"
+  @game.update_board_after_moves(choosing_player, status, state)
+
+  response.status = 201
+  response.body = @board.to_json
+end
+
+post '/move' do
+  # receive into variable
+  # {
+  #   "player": "X",
+  #   "position": "0"
+  # }
+  player_symbol = "#{params["player"]}"
+  position = "#{params["position"].to_i}"
+
+  # check if move is valid
+  @cells.map do |cell|
+    if cell != "X" || "O"
+
+      # post move to cells
+      @cells[position] = player_symbol
+
+      #update variables for response body
+      choosing_player = @game.update_player
+      status = "ok"
+      state = @game.update_state_if_winner
+      @game.update_board_after_moves(choosing_player, status, state)
+
+      #return updated gameboard
+      response.status = 200
+      response.body = @board.to_json
+
+    else
+      status = "invalid"
+      @game.update_board_after_moves(choosing_player, status, state)
+
+      response.status =  409
+      response.body =  @board.to_json
+    end
+
+end
